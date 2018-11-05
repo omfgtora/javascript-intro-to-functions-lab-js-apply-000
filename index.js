264c1007aeb3486ee2478a0175b84e9d9a2d3138
reflective-mapping-9478 <ejrobe@gmail.com>
2018-11-05 22:59:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ function shout(string){
 }
 
 functiion sayHiToGrandma(){
+  var uppercase = "HELLO!"
+ 
+  uppercase.toUpperCase() === uppercase // true
   
+  var lowercase = 'hello!'
+  
+  lowercase.toLowerCase() === lowercase // true
+  
+  var mixedCase = 'Hi there!'
+  
+  mixedCase.toLowerCase() === mixedCase // false
+  
+  mixedCase.toUpperCase() === mixedCase // false
 }
