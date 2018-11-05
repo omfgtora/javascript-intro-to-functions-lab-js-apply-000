78eb9d6a12c1ce2dfebfd9d3e9e0cb3819d558d3
reflective-mapping-9478 <ejrobe@gmail.com>
2018-11-05 23:03:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function shout(string){
 function logShout(string){
   return console.log(string.toUpperCase())
 }
+
+function logWhisper(string){
+  return console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  
+}
